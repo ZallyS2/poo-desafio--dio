095d948ab2d14f811f3735cbd00e185aca0de76d
lailaz <lailazappiello90@gmail.com>
2025-08-13 11:06:23 -0300

    feat: Projeto concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nSmartphone iPhone:");
+Smartphone iphone = new Iphone("987654321", "iPhone 12", "987654321098765", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+
+
+Console.WriteLine("\n----------------------------♡----------------------------");
